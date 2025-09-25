1af9de2d71105fe281aeee96705725659b43f366
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-09-25 14:03:54 -0700

    Chapter 05: match ctrl flow

--- Chapter6/enums/src/main.rs
+++ Chapter6/enums/src/main.rs
@@ -1,6 +1,45 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    California,
+    NewJersey,
+    Texas,
+    Washington,
+}
+
 enum IpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn plus_one(x : Option<i32>) -> Option<i32> {
+   match x {
+       None => None,
+       Some(i) => Some(i+1),
+   }
+}
+
+fn value_in_cents(coin : &Coin) {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from state: {state:?}!");
+            25
+        },
+    };
 }
 
 // Having a single message type enables genericity in functions, 
