0ca52562f875495f3230925e114140074ebacadc
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-09-27 10:32:02 -0700

    Style: apply rustfmt, whitespace revision

--- Chapter3/data_types/src/main.rs
+++ Chapter3/data_types/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let truncated = -5 / 3;
     let remainder = 43 % 5;
     let f: bool = false;
-    let z: char = 'ℤ'; 
+    let z: char = 'ℤ';
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
     println!("The value of y is {y}");
@@ -14,8 +14,8 @@ fn main() {
     let second = x.1;
     let third = x.2;
     // Array
-    let a: [i32, 5] = [1, 2, 3, 4, 5];
-    // Three instances of one 
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    // Three instances of one
     let b = [1; 3];
     let first = a[1];
 }
--- Chapter6/enums/src/main.rs
+++ Chapter6/enums/src/main.rs
@@ -9,7 +9,7 @@ enum UsState {
 }
 
 impl UsState {
-    fn existed_in(&self, year : u16) -> bool {
+    fn existed_in(&self, year: u16) -> bool {
         match self {
             UsState::Alabama => year >= 1819,
             UsState::California => year >= 1850,
@@ -30,7 +30,7 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn nested_describe_state_quarter(coin : Coin) -> Option<String> {
+fn nested_describe_state_quarter(coin: Coin) -> Option<String> {
     if let Coin::Quarter(state) = coin {
         if state.existed_in(1900) {
             Some(format!("{state:?} is decently old!"))
@@ -42,10 +42,10 @@ fn nested_describe_state_quarter(coin : Coin) -> Option<String> {
     }
 }
 
-fn describe_state_quarter(coin : Coin) -> Option<String> {
-    if let Coin::Quarter(state) = coin else {
-       return None;
-    }
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
     if state.existed_in(1900) {
         Some(format!("{state:?} is decently old!"))
     } else {
@@ -53,33 +53,33 @@ fn describe_state_quarter(coin : Coin) -> Option<String> {
     }
 }
 
-fn plus_one(x : Option<i32>) -> Option<i32> {
-   match x {
-       None => None,
-       Some(i) => Some(i+1),
-   }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
 
-fn value_in_cents(coin : &Coin) {
+fn value_in_cents(coin: &Coin) {
     match coin {
         Coin::Penny => {
             println!("Lucky penny!");
             1
-        },
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter(state) => {
             println!("State quarter from state: {state:?}!");
             25
-        },
+        }
     };
 }
 
-// Having a single message type enables genericity in functions, 
+// Having a single message type enables genericity in functions,
 // i.e. just one could be used to used to handle *all* message updates
 enum Message {
     Quit,
-    Move {x : i32, y: i32},
+    Move { x: i32, y: i32 },
     Write(String),
     ChangeColor(i32, i32, i32),
 }
@@ -94,7 +94,7 @@ fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
     let five = Some(5);
-    let absent_number : Option<i32> = None;
+    let absent_number: Option<i32> = None;
     dbg!(absent_number);
     // Optional integers don't support addition
     // let sum = five + absent_number;
