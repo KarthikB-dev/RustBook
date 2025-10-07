251c830e685d442807cf32800b2e5cae43cd559b
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-10-07 14:41:05 -0700

    Ch 9-2: various bug fixes

--- Chapter9/errors/src/main.rs
+++ Chapter9/errors/src/main.rs
@@ -1,33 +1,30 @@
-use std::io::{self, Read, ErrorKind};
+use std::io::{self, Read};
 use std::error::Error;
 use std::fs::File;
 
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
 
-
-fn last_char_of_first_line(text: &str) {
-    text.lines().next()?.chars().last();
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
     // Returns an error if this fails
     let mut username = String::new();
     let mut username_file = File::open("hello.txt")?.read_to_string(&mut username)?;
-    Ok(username);
+    Ok(username)
 }
 
 // Box is a trait object -> means any kind of error
 fn main() -> Result<(), Box<dyn Error>> {
-    // panic!("crash and burn...")
+    // panic!("crash and burn...");
     let v = vec![1, 2, 3];
-    // let oob = v[2000];
-    // println!("{oob}");
+    let oob = v[2000];
+    println!("{oob}");
     // let greeting_file_result = File::open("Hello.txt");
-
-
     // let greeting_file = match greeting_file_result {
     //     Ok(file) => file,
     //     Err(error) => match error.kind() {
@@ -40,8 +37,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     //         }
     //     },
     // };
-
     let greeting_file = File::open("hello.txt")?;
-
-    let biography = File::open("bio.txt").expect("bio.txt should be included in this project");
+    // let biography = File::open("bio.txt").expect("bio.txt should be included in this project");
+    Ok(())
 }
