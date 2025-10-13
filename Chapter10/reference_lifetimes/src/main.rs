3f0bce7ce7175606896c52c23a4ad7aa4c45f1b9
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-10-12 23:13:47 -0700

    Apply rustfmt changes to ch10

--- Chapter10/reference_lifetimes/src/main.rs
+++ Chapter10/reference_lifetimes/src/main.rs
@@ -1,21 +1,25 @@
 use std::fmt::Display;
 
-fn longest_with_an_announcement<'a, T>(
-    x: &'a str,
-    y: &'a str,
-    ann: T,
-) -> &'a str 
-where 
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
     T: Display,
 {
     println!("Announcement: {ann}");
-    if x.len() > y.len() { x } else { y }
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
-// rustc doesn't know if the value being 
+// rustc doesn't know if the value being
 // returned is x or y
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
 struct ImportantExcerpt<'a> {
@@ -53,7 +57,7 @@ fn main() {
     println!("r: {r}");
 
     let string1 = String::from("abcd");
-    let result : &str;
+    let result: &str;
     let string2 = String::from("xyz");
     let result = longest(string1.as_str(), string2.as_str());
     println!("Longest string: {result}");
