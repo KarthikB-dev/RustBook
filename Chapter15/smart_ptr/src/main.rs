cf9c9063e29e84718120b56ce94d9b2836cb2e2b
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-12-30 18:37:43 -0800

    Ch 15.2: smart pointers and regular references

---
+++ Chapter15/smart_ptr/src/main.rs
@@ -0,0 +1,36 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(x, 5);
+    assert_eq!(*y, 5);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // Version without deref coercion.
+    // It's painfully verbose...
+    hello(&(*m)[..]);
+}
