c25f85c598c48ad7bed6d2f344578b48c690478c
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2025-12-30 19:06:46 -0800

    Add Ch 15.3: reference counted smart pointers

---
+++ Chapter15/drop/src/main.rs
@@ -0,0 +1,22 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+
+fn main() {
+    let first = CustomSmartPointer {
+        data : String::from("Smart pointer 1")
+    };
+    let second = CustomSmartPointer {
+        data : String::from("Smart pointer 2")
+    };
+    println!("Two smart pointers created!");
+    // Explicit destructor calls are forbidden
+    // first.drop(); won't work!
+    drop(first);
+}
