4a4d805cd580dcdadba00a06f739d2cc9e039ab9
Karthik Bhattaram <karthik.bhattaram.1@gmail.com>
2026-01-06 23:18:18 -0800

    Style: modify indentation

--- Chapter17/async_concurrency/src/main.rs
+++ Chapter17/async_concurrency/src/main.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
-        trpl::spawn_task(async {
+        let handle = trpl::spawn_task(async {
             for i in 1..10 {
                 println!("hi number {i} from task 1!");
                 trpl::sleep(Duration::from_millis(500)).await;
@@ -12,7 +12,7 @@ fn main() {
         for i in 1..10 {
                 println!("hi number {i} from task 2!");
                 trpl::sleep(Duration::from_millis(500)).await;
-            }
         }
-    );
+        handle.await.unwrap();
+    });
 }
